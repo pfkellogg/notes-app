8dcfdf32a0e48ece8fc34223807430c97b64ebee
Patrick <pfkellogg@gmail.com>
2025-11-22 11:00:42 -0500

    reusable input components

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import TextInput from './inputs/TextInput'
+import SelectInput from './inputs/SelectInput'
+import TextAreaInput from './inputs/TextAreaInput'
 
 const NoteForm = ({ notes, setNotes }) => {
   const [formData, setFormData] = useState({
@@ -9,8 +12,6 @@ const NoteForm = ({ notes, setNotes }) => {
   })
 
   const [isFormVisible, setIsFormVisible] = useState(false)
-
-  const initialNotes = []
 
   const handleChange = e => {
     console.log('formData', { ...formData })
@@ -50,65 +51,45 @@ const NoteForm = ({ notes, setNotes }) => {
 
       {isFormVisible && (
         <form onSubmit={handleSubmit} className="mb-6">
-          <div className="mb-4">
-            <label htmlFor="title" className="block font-semibold">
-              Title
-            </label>
-            <input
-              name="title"
-              type="text"
-              className="w-full p-2 border rounded-lg"
-              value={formData.title}
-              onChange={handleChange}
-            />
-          </div>
+          <TextInput
+            label="Title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
 
-          <div className="mb-4">
-            <label htmlFor="priority" className="block font-semibold">
-              Priority
-            </label>
-            <select
-              name="priority"
-              type="text"
-              className="w-full p-2 border rounded-lg"
-              value={formData.priority}
-              onChange={handleChange}
-            >
-              <option value="High">🔴 High</option>
-              <option value="Medium">🟠 Medium</option>
-              <option value="Low">🟢 Low</option>
-            </select>
-          </div>
+          <SelectInput
+            label="Priority"
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            options={[
+              { value: 'High', label: '🔴 High' },
+              { value: 'Medium', label: '🟠 Medium' },
+              { value: 'Low', label: '🟢 Low' }
+            ]}
+          />
 
-          <div className="mb-4">
-            <label htmlFor="category" className="block font-semibold">
-              Category
-            </label>
-            <select
-              name="category"
-              type="text"
-              className="w-full p-2 border rounded-lg"
-              value={formData.category}
-              onChange={handleChange}
-            >
-              <option value="Work">📂 Work</option>
-              <option value="Personal">🏠 Personal</option>
-              <option value="Ideas">💡 Ideas</option>
-            </select>
-          </div>
+          <SelectInput
+            label="Category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            options={[
+              { value: 'Work', label: '📂 Work' },
+              { value: 'Personal', label: '🏠 Personal' },
+              { value: 'Ideas', label: '💡 Ideas' }
+            ]}
+          />
 
-          <div className="mb-4">
-            <label htmlFor="description" className="block font-semibold">
-              Description
-            </label>
-            <textarea
-              name="description"
-              type="text"
-              className="w-full p-2 border rounded-lg"
-              value={formData.description}
-              onChange={handleChange}
-            ></textarea>
-          </div>
+          <TextAreaInput
+            label="Description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
 
           <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover: bg-purple-600">
             Add Note
